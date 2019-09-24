57f06f47a9139a008abd97a2a9a926ccc64531bb
Tyler Carpenter <tcarp55@gmail.com>
2019-09-24 10:32:56 -0500

    added imports to models

--- hrapp/models/__init__.py
+++ hrapp/models/__init__.py
@@ -3,3 +3,4 @@ from .computer import Computer
 from .employee_computer import EmployeeComputer
 from .department import Department
 from .training import Training
+from .employee_training import EmployeeTraining
