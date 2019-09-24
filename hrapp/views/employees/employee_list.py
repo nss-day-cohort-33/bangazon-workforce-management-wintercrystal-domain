1ccac1e187f1a369d0b93912ea43ac9ae4f62894
Danny Barker <barkerdanny17@yahoo.com>
2019-09-24 14:43:15 -0500

    made form for creating an employee

--- hrapp/views/employees/employee_list.py
+++ hrapp/views/employees/employee_list.py
@@ -1,33 +1,46 @@
 import sqlite3
 from django.shortcuts import render
+from datetime import date
+from django.urls import reverse
+from django.shortcuts import redirect
 from hrapp.models import Employee
 from hrapp.models import model_factory
 from hrapp.models import Employee
 from ..connection import Connection
-import datetime
 
+def get_employees():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Employee)
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        select
+            e.id,
+            e.first_name,
+            e.last_name,
+            e.start_date,
+            e.is_supervisor,
+            e.department_id_id
+        from hrapp_employee e
+        """)
+        return db_cursor.fetchall()
 
 def employee_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = model_factory(Employee)
             db_cursor = conn.cursor()
+            employees = get_employees()
 
             # TODO: Add to query: e.department,
             db_cursor.execute("""
             select
-                e.id,
-                e.first_name,
-                e.last_name,
-                e.start_date,
-                e.is_supervisor,
-                e.department_id_id,
                 c.id relationId,
+                c.computer_id_id,
+                c.employee_id_id,
                 cc.id computerId,
                 cc.make,
                 cc.model
-            from hrapp_employee e
-            join hrapp_employeecomputer c on c.employee_id_id = e.id
+            from hrapp_employeecomputer c
             join hrapp_computer cc on cc.id = c.computer_id_id
             where c.unassigned_date is NULL
             """)
@@ -36,7 +49,8 @@ def employee_list(request):
 
         template = 'employees/employees_list.html'
         context = {
-            'employees': dataset
+            'computers': dataset,
+            'employees': employees
         }
 
         return render(request, template, context)
@@ -46,16 +60,17 @@ def employee_list(request):
 
         with sqlite3.connect(Connection.db_path) as conn:
             db_cursor = conn.cursor()
+            start_date = date.today().strftime("%Y/%m/%d")
 
             db_cursor.execute("""
-            INSERT INTO hrapp_department
+            INSERT INTO hrapp_employee
             (
                 first_name, last_name, start_date, is_supervisor, department_id_id
             )
-            VALUES (?, ?)
+            VALUES (?, ?, ?, ?, ?)
             """,
-            (form_data['first_name'], form_data['last_name'], ))
+            (form_data['first_name'], form_data['last_name'], start_date, form_data["supervisor"], form_data["department"] ))
 
-        return redirect(reverse('libraryapp:libraries'))
+        return redirect(reverse('hrapp:employee_list'))
 
 
