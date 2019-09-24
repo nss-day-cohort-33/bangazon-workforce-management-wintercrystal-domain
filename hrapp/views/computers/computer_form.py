843aca7d53aa1d1458b8adc76945d086e525830a
Tyler Carpenter <tcarp55@gmail.com>
2019-09-24 14:47:57 -0500

    committing to check other branch

--- hrapp/views/computers/computer_form.py
+++ hrapp/views/computers/computer_form.py
@@ -1,27 +1,46 @@
 import sqlite3
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from hrapp.models.employee import Employee
 from hrapp.models import model_factory
 from ..connection import Connection
 
+def get_employee():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Employee)
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            e.id,
+            e.first_name,
+            e.last_name
+        from hrapp_employee e
+        """)
+
+        return db_cursor.fetchall()
+
 def computer_form(request):
     if request.method == 'GET':
+        employees = get_employee()
         template = 'computers/computer_form.html'
-
-        return render(request, template, {})
-
-
-@login_required
-def book_edit_form(request, computer_id):
-
-    if request.method == 'GET':
-        book = get_book(book_id)
-        libraries = get_libraries()
-
-        template = 'books/form.html'
         context = {
-            'book': book,
-            'all_libraries': libraries
+            'all_employees': employees
         }
 
         return render(request, template, context)
+
+
+# def computer_edit_form(request, computer_id):
+
+#     if request.method == 'GET':
+#         book = get_computer(computer_id)
+#         libraries = get_libraries()
+
+#         template = 'books/form.html'
+#         context = {
+#             'book': book,
+#             'all_libraries': libraries
+#         }
+
+#         return render(request, template, context)
