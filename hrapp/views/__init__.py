8d4ab43185b0053635690e93368ecc490a3f031a
Daniel Krusch <daniel.krusch@gmail.com>
2019-09-25 11:09:33 -0500

    Training form added

--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -1,6 +1,7 @@
 from .employees.employee_list import employee_list
 from .employees.employee_form import employee_form
 from .training_programs.training_list import training_list
+from .training_programs.training_form import training_form
 from .computers.computer_list import computer_list
 from .home import home
 from .auth.logout import logout_user
--- hrapp/views/training_programs/training_list.py
+++ hrapp/views/training_programs/training_list.py
@@ -31,3 +31,19 @@ def training_list(request):
 
         return render(request, template, context)
 
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO hrapp_training
+            (
+                title, start_date, end_date, capacity
+            )
+            VALUES (?, ?, ?, ?)
+            """,
+            (form_data['title'], form_data['start_date'].format("%Y/%m/%d"), form_data['end_date'].format("%Y/%m/%d"), form_data['capacity'] ))
+
+        return redirect(reverse('hrapp:training_list'))
