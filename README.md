12f16bd951de116e8b57a92ce6275aeee11a17a3
Tyler Carpenter <tcarp55@gmail.com>
2019-09-25 15:51:10 -0500

    committing to merge master

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 ## Steps to get your project started:
 
-* Create a new repo in your chohort's github org:
-
-  * `bangazon_workforce_mgt_<your-team-name>`
-
-* Clone this repo and cd into it
-* Change your local repo's remote repo:
-
-  * `git remote remove origin`
-  * `git remote add origin <url of your team's repo>`
+* Clone down your team's repo and cd into it
 
 * Create your virtual environment:
 
--- hrapp/views/computers/computer_details.py
+++ hrapp/views/computers/computer_details.py
@@ -49,53 +49,53 @@ def computer_details(request, computer_id):
             data_set = db_cursor.fetchall()
             context = {
                 'computer': computer,
-                'data_set': data_set
+                'employee': data_set
             }
         template_name = 'computers/computer_details.html'
         return render(request, template_name, context)
 
-    elif request.method == 'POST':
-        form_data = request.POST
+    # elif request.method == 'POST':
+    #     form_data = request.POST
 
-        # Check if this POST is for editing a book
-        if (
-            "actual_method" in form_data
-            and form_data["actual_method"] == "PUT"
-        ):
-            with sqlite3.connect(Connection.db_path) as conn:
-                db_cursor = conn.cursor()
+    #     # Check if this POST is for editing a book
+    #     if (
+    #         "actual_method" in form_data
+    #         and form_data["actual_method"] == "PUT"
+    #     ):
+    #         with sqlite3.connect(Connection.db_path) as conn:
+    #             db_cursor = conn.cursor()
 
-                db_cursor.execute("""
-                UPDATE libraryapp_book
-                SET title = ?,
-                    auther = ?,
-                    ISBN_number = ?,
-                    year_published = ?,
-                    location_id = ?
-                WHERE id = ?
-                """,
-                (
-                    form_data['title'],
-                    form_data['auther'],
-                    form_data['ISBN_number'],
-                    form_data['year_published'],
-                    form_data['location'],
-                    book_id,
-                ))
+    #             db_cursor.execute("""
+    #             UPDATE libraryapp_book
+    #             SET title = ?,
+    #                 auther = ?,
+    #                 ISBN_number = ?,
+    #                 year_published = ?,
+    #                 location_id = ?
+    #             WHERE id = ?
+    #             """,
+    #             (
+    #                 form_data['title'],
+    #                 form_data['auther'],
+    #                 form_data['ISBN_number'],
+    #                 form_data['year_published'],
+    #                 form_data['location'],
+    #                 book_id,
+    #             ))
 
-            return redirect(reverse('libraryapp:books'))
+    #         return redirect(reverse('libraryapp:books'))
 
-        # Check if this POST is for deleting a book
-        if (
-            "actual_method" in form_data
-            and form_data["actual_method"] == "DELETE"
-        ):
-            with sqlite3.connect(Connection.db_path) as conn:
-                db_cursor = conn.cursor()
+    #     # Check if this POST is for deleting a book
+    #     if (
+    #         "actual_method" in form_data
+    #         and form_data["actual_method"] == "DELETE"
+    #     ):
+    #         with sqlite3.connect(Connection.db_path) as conn:
+    #             db_cursor = conn.cursor()
 
-                db_cursor.execute("""
-                    DELETE FROM libraryapp_book
-                    WHERE id = ?
-                """, (book_id,))
+    #             db_cursor.execute("""
+    #                 DELETE FROM libraryapp_book
+    #                 WHERE id = ?
+    #             """, (book_id,))
 
-            return redirect(reverse('libraryapp:books'))
+    #         return redirect(reverse('libraryapp:books'))
